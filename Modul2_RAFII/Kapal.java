932afe76acd8ca2dbcc07de3c78121f55b7cea73
RafiiDndy <rafii.dandy88@gmail.com>
2022-12-07 17:20:39 +0700

    finish

---
+++ Modul2_RAFII/Kapal.java
@@ -0,0 +1,31 @@
+package Modul2_RAFII;
+
+public class Kapal extends TransportasiAir {
+    protected String mesin;
+
+    public Kapal(int jumlahkursi,int biaya, String mesin){
+        super(jumlahkursi,biaya);
+        this.mesin = mesin;
+    }
+
+    @Override
+    public void informasi(){
+        System.out.println("Transportasi Air jenis Kapal dengan kursi "+ jumlahkursi +" ditetapkan dengan biaya sebesar Rp. "+biaya);
+    }
+
+    @Override
+    public void berlayar(){
+        System.out.println("Transportasi Air jenis Kapal sedang berlayar menggunakan mesin "+mesin+" dengan kecepatan yang tidak stabil");
+    }
+
+    public void berlayar(int kecepatan){
+        System.out.println("Transportasi Air jenis Kapal sedang berlayar menggunakan mesin "+mesin+" dengan kecepatan yang stabil di kisaran "+kecepatan+" knot");
+    }
+
+    @Override
+    public void berlabuh(){
+        System.out.println("Transportasi Air jenis Kapal berlabuh di pantai ");
+    }
+
+   
+}
